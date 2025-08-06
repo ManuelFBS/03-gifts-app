0ff6b29ccccb1d55f26227423893e8ccbd68e2a5
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-08-06 03:01:34 -0400

    Creac. de 'settings.json', 'dashboard-page.component.html', 'dashboard-page.component.ts', 'trending.component.html' y 'trending.component.ts'; Modif. 'app.routes.ts'...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+            path: 'dashboard',
+            loadComponent: () =>
+                  import(
+                        './gifts/pages/dashboard-page/dashboard-page.component'
+                  ),
+      },
+      {
+            path: 'trending',
+            loadComponent: () =>
+                  import('./gifts/pages/trending/trending.component'),
+      },
+      {
+            path: '**',
+            redirectTo: 'dashboard',
+      },
+];
