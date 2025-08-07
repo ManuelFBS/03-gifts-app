038f4dc1c145e249f9905626b0fb98a211f53be8
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-07 13:13:38 -0400

    Modif. varias...

--- src/app/gifts/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifts/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,27 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+      faChartLine,
+      faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface MenuOption {
       label: string;
       subLabel: string;
       route: string;
-      icon: string;
+      icon: any;
 }
 
 @Component({
       selector: 'gifts-side-menu-options',
-      imports: [],
+      imports: [FontAwesomeModule, RouterLink, RouterLinkActive],
       templateUrl: './side-menu-options.component.html',
 })
 export class SideMenuOptionsComponent {
       menuOptions: MenuOption[] = [
             {
-                  icon: 'fs-solid fa-chart-line',
-                  label: 'Tending',
+                  icon: faChartLine,
+                  label: 'Trending',
                   subLabel: 'Gifts Populares',
                   route: '/dashboard/trending',
             },
             {
-                  icon: 'fs-solid fa-magnifying-glass',
+                  icon: faMagnifyingGlass,
                   label: 'Buscador',
                   subLabel: 'Buscar Gifts',
                   route: '/dashboard/search',
