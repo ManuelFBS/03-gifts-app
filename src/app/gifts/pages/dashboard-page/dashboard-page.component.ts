7fbdb113a212f750e4fd2c5524d4e5a6e33ec953
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-07 09:37:58 -0400

    Modif y /o Actualiz. en 'angular.json', 'side-menu-header.component.html', 'side-menu-options.component.html', 'dashboard-page.component.html', 'dashboard-page.component.ts'...

--- src/app/gifts/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifts/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
 
 @Component({
       selector: 'app-dashboard-page',
-      imports: [RouterOutlet],
+      imports: [RouterOutlet, SideMenuHeaderComponent],
       templateUrl: './dashboard-page.component.html',
 })
 export default class DashboardPageComponent {}
