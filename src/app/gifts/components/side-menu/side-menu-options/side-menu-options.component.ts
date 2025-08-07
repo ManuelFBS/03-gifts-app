e0b867b74a56a11f1b53c7d8d254393349c71dd6
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-07 12:09:49 -0400

    Modif. en 'side-menu-options.component.html' y 'side-menu-options.component.ts'...

--- src/app/gifts/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifts/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
 
+interface MenuOption {
+      label: string;
+      subLabel: string;
+      route: string;
+      icon: string;
+}
+
 @Component({
       selector: 'gifts-side-menu-options',
       imports: [],
       templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+      menuOptions: MenuOption[] = [
+            {
+                  icon: 'fs-solid fa-chart-line',
+                  label: 'Tending',
+                  subLabel: 'Gifts Populares',
+                  route: '/dashboard/trending',
+            },
+            {
+                  icon: 'fs-solid fa-magnifying-glass',
+                  label: 'Buscador',
+                  subLabel: 'Buscar Gifts',
+                  route: '/dashboard/search',
+            },
+      ];
+}
