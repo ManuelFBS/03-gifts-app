b8bb9e33f05974402c2b1d25f453fda00926406c
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-07 09:49:33 -0400

    Creac. de 'gifts-side-menu.component.html' y 'gifts-side-menu.component.ts'; Actualiz. de 'dashboard-page.component.html'...

---
+++ src/app/gifts/components/gifts-side-menu/gifts-side-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+      selector: 'gifts-side-menu',
+      imports: [],
+      templateUrl: './gifts-side-menu.component.html',
+})
+export class GiftsSideMenuComponent {}
