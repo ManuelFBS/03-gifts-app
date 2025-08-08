96dd28f1e433e7710e84921fb4ebf572d735f90b
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-08 07:47:19 -0400

    Actualiz. de 'environment.development.ts'...

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+      production: true,
+      companyName: 'Gifts',
+      companyName2: 'App',
+      companySlogan: 'Maneja tus gifts',
+
+      //? ApiKeys
+
+      //? URLS
+};
