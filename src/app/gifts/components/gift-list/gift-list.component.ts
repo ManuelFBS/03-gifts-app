60679a8bbd84db2e9a8e40bf8f47e85fbdb944fc
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-08 08:39:26 -0400

    Creac. de 'gift-list.component.html' y 'gift-list.component.ts'...

---
+++ src/app/gifts/components/gift-list/gift-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+      selector: 'gift-list',
+      imports: [],
+      templateUrl: './gift-list.component.html',
+})
+export class GiftListComponent {}
