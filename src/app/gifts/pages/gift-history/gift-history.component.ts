3605f1d05d243de336c56e5fcccc90258fb6a699
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-11 11:55:45 -0400

    Modif. en 'gift-history.component.html' y 'gift-history.component.ts'...

--- src/app/gifts/pages/gift-history/gift-history.component.ts
+++ src/app/gifts/pages/gift-history/gift-history.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
       selector: 'gift-history',
       imports: [],
       templateUrl: './gift-history.component.html',
 })
-export default class GiftHistoryComponent {}
+export default class GiftHistoryComponent {
+      query = toSignal(
+            inject(ActivatedRoute).params.pipe(map((params) => params['query']))
+      );
+}
